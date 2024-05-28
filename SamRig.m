f2e46675e5a968713e93ab7965f9ceb1c7d4be04
ZachWang <815810395@qq.com>
2024-05-28 12:15:12 +0800

    Update SamRig.m

--- SamRig.m
+++ SamRig.m
@@ -8,18 +8,7 @@ shl=ul-cl;    %speed of left rarefaction head
 
 if s<=shl 
     %sampling point lies to the left of the rarefaction
-    D=dl;
-    U=ul;
+
 else
-    stl=ul+2*cl;
-    if s<=stl
-        %sampling point lies inside the rarefaction
-        U=(ul+2*cl+2*s)/3;
-        C=(ul+2*cl-s)/3;
-        D=C*C/g;
-    else
-        %sample point lies in right dry-bed state
-        D=dr;
-        U=ur;
-    end
+
 end
